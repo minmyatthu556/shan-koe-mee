0f64937b5cfee07e8ea24ce4b3b5e415e7e18724
minmyatthu <mmthu17mce@ytu.edu.mm>
2021-05-23 19:56:38 +0630

    add function to draw a random card

--- build/index.js
+++ build/index.js
@@ -1,6 +1,7 @@
 "use strict";
 const suits = ['spades', 'hearts', 'diamonds', 'clubs'];
-const numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9'];
+const numbers = ['A', 'K', 'Q', 'J', '2', '3', '4', '5', '6', '7', '8', '9'];
+// function to build a deck of cards (eg. [spades, A])
 const buildADeck = () => {
     let deck = [];
     for (let i = 0; i < suits.length; i++) {
@@ -10,5 +11,17 @@ const buildADeck = () => {
     }
     return deck;
 };
-const deck = buildADeck();
+let deck = buildADeck();
 console.log(deck);
+const generateRandomNumber = (num) => {
+    return Math.floor(Math.random() * num);
+};
+const drawACard = () => {
+    const randomIndex = generateRandomNumber(48);
+    const chosenCard = deck[randomIndex];
+    deck = deck.filter(card => card !== chosenCard);
+    return chosenCard;
+};
+const chosenCard = drawACard();
+console.log(deck);
+console.log(chosenCard);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 const suits = ['spades', 'hearts', 'diamonds', 'clubs']
-const numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9']
+const numbers = ['A', 'K', 'Q', 'J', '2', '3', '4', '5', '6', '7', '8', '9']
 
-const buildADeck = () => {
+// function to build a deck of cards (eg. [spades, A])
+const buildADeck = (): string[][] => {
   let deck: string[][] = []
   for (let i = 0; i < suits.length; i++) {
     for (let j = 0; j < numbers.length; j++) {
@@ -11,5 +12,18 @@ const buildADeck = () => {
   return deck
 }
 
-const deck = buildADeck()
-console.log(deck)
+let deck = buildADeck()
+
+// function to generate random number from 0 to assigned parameter
+const generateRandomNumber = (num: number): number => {
+  return Math.floor(Math.random() * num)
+}
+
+// function to return a random card from deck and remove that card from deck
+const drawACard = (): string[] => {
+  const randomIndex = generateRandomNumber(48)
+  const chosenCard = deck[randomIndex]
+
+  deck = deck.filter((card) => card !== chosenCard)
+  return chosenCard
+}
