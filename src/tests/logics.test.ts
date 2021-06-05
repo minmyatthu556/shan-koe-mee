45d925b58f2e7148020a36d426cf5e61c063f390
minmyatthu <mmthu17mce@ytu.edu.mm>
2021-06-06 00:29:04 +0630

    fix bugs

--- src/tests/logics.test.ts
+++ src/tests/logics.test.ts
@@ -5,6 +5,8 @@ import {
   GameCards,
 } from '../utils/logics'
 
+import { findTheBiggestNum } from '../utils/helpers'
+
 describe('The game should return the card with the bigger suit', () => {
   describe('With two cards', () => {
     test('with two different cards', () => {
@@ -12,6 +14,12 @@ describe('The game should return the card with the bigger suit', () => {
     })
     test('with two cards with same number', () => {
       expect(findBiggerSuit(['♥️', '5'], ['♠️', '5'])).toBe('♠️')
+    })
+    test('test for svlete', () => {
+      expect(findBiggerSuit(['♣️', '9'], ['♦️', 'Q'])).toBe('♦️')
+    })
+    test('test for svelte', () => {
+      expect(findBiggerSuit(['♥️', '6'], ['♠️', '3'])).toBe('♥️')
     })
   })
   describe('With three cards', () => {
@@ -64,35 +72,94 @@ describe('The game should return the number score of the two or three cards', ()
   })
 })
 
+describe('should return the one with bigger number card', () => {
+  test('should return user number card', () => {
+    expect(findTheBiggestNum(['♣️', '9'],['♦️', 'Q'],undefined)).toBe(9)
+  })
+  test('should return bot number card', () => {
+    expect(findTheBiggestNum(['♥️', '6'],['♠️', '3'],undefined)).toBe(4)
+  })
+})
+
 describe('The game should return the winner', () => {
   test('the game with no edge cases', () => {
     const gameCards: GameCards = {
       userNum: 5,
-      userSuit: '♥️',
-      botNum: 3,
-      botSuit: '♠️',
+      userSuit: '♦️',
+      botNum: 9,
+      botSuit: '♥️',
       userFirst: ['♥️', 'A'],
       userSecond: ['♥️', '4'],
+      userThird: undefined,
+      botThird: undefined,
       botFirst: ['♠️', '9'],
       botSecond: ['♠️', '4'],
     }
-    expect(gameLogic(gameCards)).toBe('User')
-  })
-  test('when the number score is the same', () => {
-    const gameCards: GameCards = {
-      userNum: 5,
-      userSuit: '♥️',
-      botNum: 5,
-      botSuit: '♠️',
-      userFirst: ['A', '♥️'],
-      userSecond: ['4', '♥️'],
-      botFirst: ['3', '♠️'],
-      botSecond: ['2', '♠️'],
-    }
     expect(gameLogic(gameCards)).toBe('Bot')
-  })
+  }),
   describe('when the scores are the same', () => {
-    test('when the bot has bigger number card', () => {
+    test('when both have two cards and user has bigger number', () => {
+      const gameCards: GameCards = 
+        {
+          userNum: 9,
+          userSuit: '♦️',
+          botNum: 9,
+          botSuit: '♥️',
+          userFirst: ['♣️', '9'],
+          userSecond: ['♦️', 'Q'],
+          botFirst: ['♥️', '6'],
+          botSecond: ['♠️', '3'],
+          userThird: undefined,
+        botThird: undefined,
+        }
+        expect(gameLogic(gameCards)).toBe('User')
+    })
+    test('when both have two cards and bot has bigger number', () => {
+      const gameCards: GameCards = {
+        userNum: 5,
+        userSuit: '♥️',
+        botNum: 5,
+        botSuit: '♠️',
+        botFirst: ['♥️', 'A'],
+        botSecond: ['♥️', '4'],
+        userFirst: ['♠️', '3'],
+        userSecond: ['♠️', '2'],
+        userThird: undefined,
+        botThird: undefined,
+      }
+      expect(gameLogic(gameCards)).toBe('Bot')
+    })
+    test('when the user has only two cards and the bot has three', () => {
+      const gameCards: GameCards = {
+        userNum: 5,
+        userSuit: '♥️',
+        botNum: 5,
+        botSuit: '♠️',
+        userFirst: ['♥️', '2'],
+        userSecond: ['♥️', '3'],
+        botFirst: ['♠️', 'J'],
+        userThird: undefined,
+        botSecond: ['♠️', '4'],
+        botThird: ['♥️', 'A'],
+      }
+      expect(gameLogic(gameCards)).toBe('User')
+    })
+    test('when the bot has only two cards and the user has three', () => {
+      const gameCards: GameCards = {
+        userNum: 5,
+        userSuit: '♥️',
+        botNum: 5,
+        botSuit: '♠️',
+        userFirst: ['♥️', '2'],
+        userSecond: ['♥️', '3'],
+        botFirst: ['♠️', 'J'],
+        botThird: undefined,
+        botSecond: ['♠️', '4'],
+        userThird: ['♥️', 'A'],
+      }
+      expect(gameLogic(gameCards)).toBe('Bot')
+    })
+    test('when both have three cards the bot has bigger number card', () => {
       const gameCards: GameCards = {
         userNum: 5,
         userSuit: '♥️',
@@ -107,7 +174,7 @@ describe('The game should return the winner', () => {
       }
       expect(gameLogic(gameCards)).toBe('Bot')
     })
-    test('when the user has bigger number card', () => {
+    test('when both have three cards and the user has bigger number card', () => {
       const gameCards: GameCards = {
         userNum: 7,
         userSuit: '♥️',
@@ -119,6 +186,21 @@ describe('The game should return the winner', () => {
         botFirst: ['♠️', '3'],
         botSecond: ['♠️', '4'],
         botThird: ['♥️', 'J'],
+      }
+      expect(gameLogic(gameCards)).toBe('User')
+    })
+    test('when both have same biggest number cards and the user has bigger suit', () => {
+      const gameCards: GameCards = {
+        userNum: 7,
+        userSuit: '♠️',
+        botNum: 7,
+        botSuit: '♥️',
+        userFirst: ['♥️', '6'],
+        userSecond: ['♥️', 'K'],
+        userThird: ['♠️', 'A'],
+        botFirst: ['♠️', '2'],
+        botSecond: ['♠️', '4'],
+        botThird: ['♥️', 'A'],
       }
       expect(gameLogic(gameCards)).toBe('User')
     })
--- src/utils/logics.ts
+++ src/utils/logics.ts
@@ -1,7 +1,7 @@
-import { turnKQJto10, findTheBiggestNum } from './helpers'
+import { turnKQJto10, findTheBiggestNum } from './helpers';
 
-const suits = ['♣️', '♦️', '♥️', '♠️']
-const numbers = ['2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K', 'A']
+const suits = ['♣️', '♦️', '♥️', '♠️'];
+const numbers = ['2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K', 'A'];
 
 // find the suit of the card with largest number
 export const findBiggerSuit = (
@@ -14,13 +14,13 @@ export const findBiggerSuit = (
       const cardWithBiggerNum =
         numbers.indexOf(firstCard[1]) > numbers.indexOf(secondCard[1])
           ? firstCard
-          : secondCard
-      return cardWithBiggerNum[0]
+          : secondCard;
+      return cardWithBiggerNum[0];
     } else {
       if (suits.indexOf(firstCard[0]) > suits.indexOf(secondCard[0])) {
-        return firstCard[0]
+        return firstCard[0];
       } else {
-        return secondCard[0]
+        return secondCard[0];
       }
     }
   } /* when the third card exists */ else {
@@ -30,57 +30,57 @@ export const findBiggerSuit = (
         suits.indexOf(firstCard[0]),
         suits.indexOf(secondCard[0]),
         suits.indexOf(thirdCard[0])
-      )
+      );
       if (max === suits.indexOf(firstCard[0])) {
-        return firstCard[0]
+        return firstCard[0];
       } else if (max === suits.indexOf(secondCard[0])) {
-        return secondCard[0]
+        return secondCard[0];
       } else {
-        return thirdCard[0]
+        return thirdCard[0];
       }
     } /* if two cards have same number */ else if (
       firstCard[1] === secondCard[1] &&
       numbers.indexOf(thirdCard[1]) < numbers.indexOf(secondCard[1])
     ) {
       if (suits.indexOf(firstCard[0]) > suits.indexOf(secondCard[0])) {
-        return firstCard[0]
+        return firstCard[0];
       } else {
-        return secondCard[0]
+        return secondCard[0];
       }
     } else if (
       firstCard[1] === thirdCard[1] &&
       numbers.indexOf(secondCard[1]) < numbers.indexOf(firstCard[1])
     ) {
       if (suits.indexOf(firstCard[0]) > suits.indexOf(thirdCard[0])) {
-        return firstCard[0]
+        return firstCard[0];
       } else {
-        return thirdCard[0]
+        return thirdCard[0];
       }
     } else if (
       secondCard[1] === thirdCard[1] &&
       numbers.indexOf(firstCard[1]) < numbers.indexOf(secondCard[1])
     ) {
       if (suits.indexOf(secondCard[0]) > suits.indexOf(thirdCard[0])) {
-        return secondCard[0]
+        return secondCard[0];
       } else {
-        return thirdCard[0]
+        return thirdCard[0];
       }
     } else {
       const max = Math.max(
         numbers.indexOf(firstCard[1]),
         numbers.indexOf(secondCard[1]),
         numbers.indexOf(thirdCard[1])
-      )
+      );
       if (max === numbers.indexOf(firstCard[1])) {
-        return firstCard[0]
+        return firstCard[0];
       } else if (max === numbers.indexOf(secondCard[1])) {
-        return secondCard[0]
+        return secondCard[0];
       } else {
-        return thirdCard[0]
+        return thirdCard[0];
       }
     }
   }
-}
+};
 
 // find the score of the number of the cards
 export const findNumberScore = (
@@ -88,46 +88,45 @@ export const findNumberScore = (
   secondCard: string[],
   thirdCard: string[] | null = null
 ): number => {
-  let numA: number
-  let numB: number
-  let numC = 0
+  let numA: number;
+  let numB: number;
+  let numC = 0;
 
-  numA = turnKQJto10(firstCard)
-  numB = turnKQJto10(secondCard)
+  numA = turnKQJto10(firstCard);
+  numB = turnKQJto10(secondCard);
 
   if (thirdCard) {
-    numC = turnKQJto10(thirdCard)
+    numC = turnKQJto10(thirdCard);
   }
 
-  let sum = numA + numB + numC
+  let sum = numA + numB + numC;
 
   if (sum === 30) {
-    sum = 0
+    sum = 0;
   } else if (sum > 19) {
-    sum -= 20
+    sum -= 20;
   } else if (sum > 9 && sum < 20) {
-    sum -= 10
+    sum -= 10;
   }
-  return sum
-}
+  return sum;
+};
 
 export interface GameCards {
-  userNum: number
-  userSuit: string
-  botNum: number
-  botSuit: string
-  userFirst: string[]
-  userSecond: string[]
-  userThird?: string[]
-  botFirst: string[]
-  botSecond: string[]
-  botThird?: string[]
+  userNum: number;
+  userSuit: string;
+  botNum: number;
+  botSuit: string;
+  userFirst: string[];
+  userSecond: string[];
+  userThird: string[] | undefined;
+  botFirst: string[];
+  botSecond: string[];
+  botThird: string[] | undefined;
 }
 
 // decide the winner
 export const gameLogic = (gameCards: GameCards): string => {
-  let winner: string = ''
-  
+  let winner: string = '';
   const {
     userNum,
     botNum,
@@ -139,48 +138,53 @@ export const gameLogic = (gameCards: GameCards): string => {
     botFirst,
     botSecond,
     botThird,
-  } = gameCards
-
-  let userBiggestNum: number
-  let botBiggestNum: number
+  } = gameCards;
+  let userBiggestNum: number;
+  let botBiggestNum: number;
 
   if (userThird) {
-    userBiggestNum = findTheBiggestNum(userFirst, userSecond, userThird)
+    userBiggestNum = findTheBiggestNum(userFirst, userSecond, userThird);
   } else {
-    userBiggestNum = findTheBiggestNum(userFirst, userSecond, undefined)
+    userBiggestNum = findTheBiggestNum(userFirst, userSecond, undefined);
   }
 
   if (botThird) {
-    botBiggestNum = findTheBiggestNum(botFirst, botSecond, botThird)
+    botBiggestNum = findTheBiggestNum(botFirst, botSecond, botThird);
   } else {
-    botBiggestNum = findTheBiggestNum(botFirst, botSecond, undefined)
-  }
-    
-
-  if (userNum === botNum && userSuit === botSuit) {
-    if(userBiggestNum > botBiggestNum) {
-      winner = 'User'
-    } else {
-      winner = 'Bot'
-    }
+    botBiggestNum = findTheBiggestNum(botFirst, botSecond, undefined);
   }
 
-  if ((userNum === 8 || userNum === 9) && botNum < 8) {
-    winner = 'User'
+  
+  if ((userNum === 8 || userNum === 9) && botNum < 8) {   //autowin
+    winner = 'User';
   } else if ((botNum === 8 || botNum === 9) && userNum < 8) {
-    winner = 'Bot'
-  } else if (userNum > botNum) {
-    winner = 'User'
+    winner = 'Bot';
+  } else if (userNum > botNum) {   // normal condition
+    winner = 'User';
   } else if (botNum > userNum) {
-    winner = 'Bot'
-  } else if (userNum === botNum) {
-    if (suits.indexOf(gameCards.userSuit) > suits.indexOf(gameCards.botSuit)) {
-      winner = 'User'
-    } else if (
-      suits.indexOf(gameCards.botSuit) > suits.indexOf(gameCards.userSuit)
-    ) {
-      winner = 'Bot'
+    winner = 'Bot';
+  } else if (userNum === botNum) {   // if they have same number score
+    if (!userThird && botThird) {    // if user has two cards and bot as three and vice versa
+      winner = 'User';
+    } else if (!botThird && userThird) {
+      winner = 'Bot';
+    } else {
+      if (userBiggestNum > botBiggestNum) {            // if both have two or three cards each
+        winner = 'User';
+      } else if (botBiggestNum > userBiggestNum) {
+        winner = 'Bot';
+      } else {
+        if (
+          suits.indexOf(gameCards.userSuit) > suits.indexOf(gameCards.botSuit)
+        ) {
+          winner = 'User';
+        } else if (
+          suits.indexOf(gameCards.botSuit) > suits.indexOf(gameCards.userSuit)
+        ) {
+          winner = 'Bot';
+        }
+      }
     }
   }
-  return winner
-}
+  return winner;
+};
